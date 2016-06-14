51caf3173b8319239379273882e3a432fbf4ef77
Fauxpaw <fauxpawsc@live.com>
2016-06-13 20:00:07 -0700

    refactored to generic func

--- michael/codeChallenge/ReverseAnArray.playground/Contents.swift
+++ michael/codeChallenge/ReverseAnArray.playground/Contents.swift
@@ -2,32 +2,53 @@
 
 import UIKit
 
+//challenge: reverse the order of elements in an array
+
 var myArray = ["Earth", "Fire", "Water", "Air", "Spirit"]
 var secondArray = ["Baseball", "Football", "Hockey", "Badminton", "Ultimate Frisbee", "Water Polo", "Basketball"]
+//func printTwoIndexes(input: [String])-> [String]{
+//    
+//    var newArray = input
+//    var startIndex = 0
+//    var endIndex = newArray.count-1
+//    var temp = ""
+//    for _ in 0...newArray.count/2 {
+//            temp = newArray[startIndex]
+//            newArray[startIndex] = newArray[endIndex]
+//            newArray[endIndex] = temp
+//            startIndex++
+//            endIndex--
+//    }
+//    return(newArray)
+//}
 
-func printTwoIndexes(input: [String])-> [String]{
-   
+
+//use only generics to accomplish the same task
+
+func printTwoIndexes<T>(input: [T]){
+    
     var newArray = input
     var startIndex = 0
     var endIndex = newArray.count-1
-    var temp = ""
-    var maxIterationCount = newArray.count/2
-    
-    for _ in newArray {
-        while maxIterationCount > 0{
-            temp = newArray[startIndex]
-            newArray[startIndex] = newArray[endIndex]
-            newArray[endIndex] = temp
-            startIndex++
-            endIndex--
-            maxIterationCount--
-        }
+    var temp: T
+    for _ in 0...newArray.count/2 {
+        temp = newArray[startIndex]
+        newArray[startIndex] = newArray[endIndex]
+        newArray[endIndex] = temp
+        startIndex++
+        endIndex--
     }
-    
-return(newArray)
+    print(newArray)
 }
 
+let intArray = [1,2,3,4,5]
+let boolArray = [true, true, true, false, false]
+
+printTwoIndexes(intArray)
+printTwoIndexes(boolArray)
 printTwoIndexes(myArray)
 printTwoIndexes(secondArray)
+
+//best solution I can create is O of n/2
 
 
